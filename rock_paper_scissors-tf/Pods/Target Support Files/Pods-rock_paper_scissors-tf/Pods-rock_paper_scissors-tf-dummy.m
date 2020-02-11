935175f346906a0a4a5f60df704044bf81a2ddae
pnzr00t <pnz.r00t@gmail.com>
2020-02-11 18:53:46 +0300

    RockPaperScissor - TensorFlowLite train project

---
+++ rock_paper_scissors-tf/Pods/Target Support Files/Pods-rock_paper_scissors-tf/Pods-rock_paper_scissors-tf-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rock_paper_scissors_tf : NSObject
+@end
+@implementation PodsDummy_Pods_rock_paper_scissors_tf
+@end
---
+++ rock_paper_scissors-tf/rock_paper_scissors-tfTests/rock_paper_scissors_tfTests.swift
@@ -0,0 +1,34 @@
+//
+//  rock_paper_scissors_tfTests.swift
+//  rock_paper_scissors-tfTests
+//
+//  Created by 	Oleg2 on 11.02.2020.
+//  Copyright © 2020 Oleg Pustoshkin. All rights reserved.
+//
+
+import XCTest
+@testable import rock_paper_scissors_tf
+
+class rock_paper_scissors_tfTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
